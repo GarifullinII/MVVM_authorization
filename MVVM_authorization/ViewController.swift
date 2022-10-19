40c09a2ec01eb0e3c3a521dc23f96b098653f59c
GArifullinII <ildaridrisovich@gmail.com>
2022-10-19 23:19:09 +0400

    Initial Commit

---
+++ MVVM_authorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_authorization
+//
+//  Created by Ildar Garifullin on 19/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
